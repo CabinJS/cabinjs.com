20294972a29cc09a60272e73cf39320c4b244e42
ChrisWren <cthewren@gmail.com>
2013-08-30 18:25:27 -0700

    Updates

--- scripts/main.js
+++ scripts/main.js
@@ -3,4 +3,9 @@ $(function() {
   $header.css({
     marginBottom: $(window).height() + 10 - $header.height()
   });
+  $('.cli-code').on('click', function () {
+  	var $cliCode = $(this);
+    $cliCode.focus();
+    $cliCode.select();
+  });
 });
