c032666cb8c96542c9abc779227c820cf519ba1d
ChrisWren <cthewren@gmail.com>
2013-08-31 18:12:29 -0700

    Added deploy task for deploying to GitHub pages

--- Gruntfile.js
+++ Gruntfile.js
@@ -84,6 +84,11 @@ module.exports = function (grunt) {
     }
   });
 
+  grunt.registerTask('deploy', [
+    'build',
+    'gh-pages'
+  ]);
+
   grunt.registerTask('build', [
     'clean',
     'compass',
