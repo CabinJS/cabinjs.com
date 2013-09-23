cbcda43e864dd27c46ef52db9a35d410c9d74669
Colin Wren <colinwrenca@gmail.com>
2013-09-22 21:25:35 -0700

    Updates

--- scripts/main.js
+++ scripts/main.js
@@ -1,12 +1,6 @@
 $(function() {
-  var $header = $('header');
-  $header.css({
-    marginBottom: $(window).height() + 10 - $header.height()
-  });
-
   $('.cli-code').on('click', function () {
-    var $cliCode = $(this);
-    $cliCode
+    $(this)
       .focus()
       .select();
   });
