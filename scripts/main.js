02ba8098d9ea80065cf8b2168180b14210f97093
ChrisWren <cthewren@gmail.com>
2013-09-25 21:18:06 -0700

    Updates

--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,23 @@
 $(function() {
-  $('.cli-code').on('click', function () {
-    $(this)
-      .focus()
-      .select();
+
+  $('code[class^=lang]').click(function () {
+    selectText($(this)[0]);
   });
+
+  function selectText(element) {
+      var doc = document
+          , range, selection
+      ;
+      if (doc.body.createTextRange) { //ms
+          range = doc.body.createTextRange();
+          range.moveToElementText(element);
+          range.select();
+      } else if (window.getSelection) { //all others
+          selection = window.getSelection();
+          range = doc.createRange();
+          range.selectNodeContents(element);
+          selection.removeAllRanges();
+          selection.addRange(range);
+      }
+  }
 });
